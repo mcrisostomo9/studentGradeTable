29181da1ed3e55a022c4dd485cff2beaf2d46815
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-05-06 00:01:01 -0700

    fixed bootstrap class

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,9 +41,10 @@ export default class Header extends Component {
   render(){
     return(
       <div className="page-header">
-        <h1 className="hidden-xs hidden-sm">Student Grade Table
+        <h1 className="hidden-xs hidden-sm visible-md-block visible-lg-block">Student Grade Table
           <small className="pull-right">Grade Average: <span className="label label-default">{this.state.gradeAverage}%</span></small>
         </h1>
+
         <h3 className="hidden-md hidden-lg">Student Grade Table
           <small className="pull-right">Grade Average:
           <span className="label label-default">{this.state.gradeAverage}%</span></small>
