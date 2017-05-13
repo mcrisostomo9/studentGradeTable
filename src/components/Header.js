9b1b18ccd59294899194ce349a10c7350d70cf42
Mark <mcrisostomo9@gmail.com>
2017-05-12 20:07:36 -0700

    deleted unused css file

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return(
       <div className="page-header">
         <h1 className="hidden-xs hidden-sm visible-md-block visible-lg-block">Student Grade Table
-          <small className="pull-right">Grade Average: <span className="label label-default">{props.gradeAverage}%</span></small>
+          <small className="pull-right">Grade Averages: <span className="label label-default">{`${props.gradeAverage}%`}</span></small>
         </h1>
 
         <h3 className="hidden-md hidden-lg">Student Grade Table
